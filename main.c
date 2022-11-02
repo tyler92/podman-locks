484b66a1e42164403d277807458ce80d11b8d1b9
Mikhail Khachayants <khachayants@arrival.com>
2022-11-02 20:12:27 +0300

    Improve formatting

--- main.c
+++ main.c
@@ -6,37 +6,36 @@
 
 static int get_owner_thread(const pthread_mutex_t *mutex)
 {
-    // NOTE: It's not portable way at all
-    return mutex->__data.__owner;
+	// NOTE: It's not portable way at all
+	return mutex->__data.__owner;
 }
 
 int main()
 {
 	int error = 0;
 	shm_struct_t *mem;
-	struct __pthread_mutex_s *mutex_data;
 	int owner;
 
-    int uid = geteuid();
-    char shm_buffer[PATH_MAX];
-    const char *shm_path = NULL;
+	int uid = geteuid();
+	char shm_buffer[PATH_MAX];
+	const char *shm_path = NULL;
 
-    if(uid != 0) {
-        sprintf(shm_buffer, "libpod_rootless_lock_%d", uid);
-        shm_path = shm_buffer;
-    } else {
-        shm_path = "/libpod_lock";
-    }
+	if(uid != 0) {
+		sprintf(shm_buffer, "libpod_rootless_lock_%d", uid);
+		shm_path = shm_buffer;
+	} else {
+		shm_path = "/libpod_lock";
+	}
 
-    printf("Open %s ...\n", shm_path);
-    mem = open_lock_shm(strdup(shm_path), 2048, &error);
+	printf("Open %s ...\n", shm_path);
+	mem = open_lock_shm(strdup(shm_path), 2048, &error);
 
 	if(!mem) {
 		printf("Failed to open %s: %s\n", shm_path, strerror(-error));
 		return 1;
 	}
 
-    owner = get_owner_thread(&mem->segment_lock);
+	owner = get_owner_thread(&mem->segment_lock);
 
 	if(owner) {
 		printf("Segment lock owner is thread %d\n", owner);
@@ -44,7 +43,7 @@ int main()
 
 	for(int i = 0, k = 0; i < 2048 / BITMAP_SIZE; ++i) {
 		for(int j = 0; j < BITMAP_SIZE; ++j) {
-            owner = get_owner_thread(&mem->locks[i].locks[j]);
+			owner = get_owner_thread(&mem->locks[i].locks[j]);
 
 			if(owner) {
 				printf("Lock #%d owner is thread %d\n", k, owner);
